e4fedbb66e8fac56982b1a38aac15e27221d95ac
mohamed khaled <moham35356@gmail.com>
2025-07-03 18:21:25 +0300

    init

---
+++ src/main/java/com/example/order_service/MessageDTO.java
@@ -0,0 +1,49 @@
+package com.example.order_service;
+
+public class MessageDTO {
+    private String orderId;
+    private String productId;
+    private int quantity;
+    private String customerId;
+    private String status;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
